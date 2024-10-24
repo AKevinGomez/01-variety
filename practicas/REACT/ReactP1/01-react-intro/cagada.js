364b508f169f40580ec115cd9e25da9e4fd523c1
AKevinGomez <kevinalejandrogomez111@gmail.com>
2024-10-24 16:37:07 -0300

    primer commit

---
+++ practicas/REACT/ReactP1/01-react-intro/cagada.js
@@ -0,0 +1,49 @@
+// const sonic = {
+//     color: 'azul',
+//     nombre: 'Sonic',
+//     esmeraldas: 0,
+//     rings: 100,
+//     vidas: 5
+// }
+
+// const shadow = {
+//     color: 'negro y rojo',
+//     nombre: 'Shadow',
+//     esmeraldas: 1,
+//     rings: 110,
+//     vidas: 5
+// }
+
+// esmEnNivel = 1;
+
+// const esmObtenidas = ({ esmeraldas, nombre }) => {
+
+//     console.log("no se que onda tu codigo pero salio bien,", nombre);
+
+//     esmSumada = esmeraldas + esmEnNivel;
+
+//     return esmSumada + " esmeraldas"
+// };
+
+// console.log(esmObtenidas(sonic));
+
+
+const personajes = ['p1', 'p2', 'p3'];
+
+const retornaArreglo = () => {
+    
+    return['ABC', 123]
+}
+
+const [letras, numeros] = retornaArreglo();
+
+
+
+const useState = ( valor ) => {
+    return [ valor, () => { console.log('SI PAPA')
+    }]
+}
+
+const arr = useState ( 'p1' )
+
+console.log(nombre);
